ee396e81b3313e46659936d341b5c98fdcbdef11
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2024-12-11 07:39:01 +0900

    gpt2

---
+++ openai-gpt2/export-model.py
@@ -0,0 +1,63 @@
+import coremltools as ct
+import numpy as np
+import torch
+from transformers.models.gpt2.modeling_gpt2 import GPT2LMHeadModel
+
+model_id = "gpt2"
+
+batch_size = 1
+context_size = 128
+input_shape = (batch_size, context_size)
+
+
+# print(model.__class__.__name__)
+
+
+class BaselineGPT2LMHeadModel(GPT2LMHeadModel):
+    """Baseline LlamaForCausalLM model without key/value caching."""
+
+    @torch.no_grad()
+    def forward(
+        self,
+        input_ids: torch.LongTensor,
+        attention_mask: torch.LongTensor,
+    ) -> torch.Tensor:
+        out = super().forward(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            use_cache=False,
+        )
+        return out.logits
+
+
+torch_model = BaselineGPT2LMHeadModel.from_pretrained(model_id).eval()
+
+# trace the PyTorch model
+example_inputs: tuple[torch.Tensor, torch.Tensor] = (
+    torch.zeros(input_shape, dtype=torch.int32),
+    torch.zeros(input_shape, dtype=torch.int32),
+)
+traced_model: torch.jit.ScriptModule = torch.jit.trace(
+    torch_model,
+    example_inputs=example_inputs,
+)
+
+# GPT2LMHeadModel
+# input_ids torch.Size([1, 11])
+# attention_mask torch.Size([1, 11])
+# convert to Core ML format
+inputs: list[ct.TensorType] = [
+    ct.TensorType(shape=input_shape, dtype=np.int32, name="inputIds"),
+    ct.TensorType(shape=input_shape, dtype=np.int32, name="attentionMask"),
+]
+
+outputs: list[ct.TensorType] = [ct.TensorType(dtype=np.float16, name="logits")]
+mlmodel: ct.models.MLModel = ct.convert(
+    traced_model,
+    inputs=inputs,
+    outputs=outputs,
+    minimum_deployment_target=ct.target.macOS13,
+    skip_model_load=True,
+)
+
+mlmodel.save("models/gpt2.mlpackage")
---
+++ openai-gpt2/predict.py
@@ -0,0 +1,71 @@
+import coremltools as ct
+import torch
+from transformers import AutoTokenizer
+
+batch_size = 1
+context_size = 128
+model_id = "gpt2"
+
+loaded_model = ct.models.MLModel("models/gpt2.mlpackage")
+tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
+
+# prompt
+prompt = "What is generative AI?"
+inputs = tokenizer(prompt, return_tensors="pt", max_length=2048, truncation=True)
+
+input_ids = inputs["input_ids"]
+attention_mask = inputs["attention_mask"]
+
+
+# パディング関数を定義
+def pad_to_context_size(tensor, context_size, pad_value=0):
+    padded_tensor = torch.zeros((tensor.size(0), context_size), dtype=tensor.dtype)
+    padded_tensor[:, : tensor.size(1)] = tensor
+    return padded_tensor
+
+
+# 生成ループの設定
+max_generated_tokens = 50  # 最大生成トークン数
+generated_tokens = []  # 生成されたトークンを格納
+
+for i in range(max_generated_tokens):
+    # print(i, input_ids)
+
+    # 現在の input_ids と attention_mask をコンテキストサイズにパディング
+    input_ids_padded = pad_to_context_size(input_ids, context_size).to(torch.int32)
+    attention_mask_padded = pad_to_context_size(attention_mask, context_size).to(
+        torch.int32
+    )
+
+    # CoreMLモデルに入力
+    logits = loaded_model.predict(
+        {"inputIds": input_ids_padded, "attentionMask": attention_mask_padded}
+    )
+
+    # ロジットから次のトークンを取得 (貪欲法: 最大値のインデックスを選択)
+    next_token_id = torch.tensor(
+        [logits["logits"][0, input_ids.size(1) - 1, :].argmax()]
+    )
+
+    # <eos>トークンで終了チェック
+    if next_token_id.item() == tokenizer.eos_token_id:
+        break
+
+    # 生成されたトークンを記録
+    generated_tokens.append(next_token_id.item())
+
+    # input_idsとattention_maskを更新
+    input_ids = torch.cat([input_ids, next_token_id.unsqueeze(0)], dim=1)
+    attention_mask = torch.cat(
+        [attention_mask, torch.ones((1, 1), dtype=torch.long)], dim=1
+    )
+
+    # コンテキストサイズを超えないようにする
+    if input_ids.size(1) > context_size:
+        input_ids = input_ids[:, -context_size:]
+        attention_mask = attention_mask[:, -context_size:]
+
+# 生成されたトークンをデコードしてテキストに変換
+generated_text = tokenizer.decode(generated_tokens, skip_special_tokens=True)
+
+print("Generated Text:", generated_text)
