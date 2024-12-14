9e0752b29a51c02e0497ea494ba96fd818dca334
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2024-12-14 20:17:45 +0900

    Refactor GPT-2 CLI to enhance token generation and model loading

--- MyCoreMLRunner/Sources/GPT2ModelCLI/main.swift
+++ MyCoreMLRunner/Sources/GPT2ModelCLI/main.swift
@@ -1,6 +1,107 @@
 import CoreML
 import Foundation
 import GPT2Model
+import Generation
+import Models
+import Tokenizers
+
+let maxContextLength = 1024
+
+class Generator: Generation {
+    private let model: MLModel
+
+    private let maxContextLength: Int
+
+    init(model: MLModel, maxContextLength: Int) {
+        self.model = model
+        self.maxContextLength = maxContextLength
+    }
+
+    public func predictNextTokenScores(
+        _ tokens: MLTensor,
+        config: GenerationConfig
+    ) async -> MLTensor {
+        assert(tokens.rank == 2)  // [batch, current sequence length]
+        let tokenCount = tokens.shape[1]
+        let padLength = self.maxContextLength - tokenCount
+
+        var inputIds = tokens
+        if padLength > 0 {
+            let padding = MLTensor(repeating: Int32(config.padTokenId ?? 0), shape: [1, padLength])
+            inputIds = MLTensor(concatenating: [tokens, padding], alongAxis: -1)
+        }
+
+        var inputDictionary = ["inputIds": inputIds]
+
+        var mask = [Int32](repeating: 1, count: tokenCount)
+
+        // padLength 分だけ 0 を追加する
+        if padLength > 0 {
+            mask += [Int32](repeating: 0, count: padLength)
+        }
+
+        let attentionMask = MLTensor(shape: inputIds.shape, scalars: mask)
+        inputDictionary["attentionMask"] = attentionMask
+
+        let outputs = try! await model.prediction(from: inputDictionary)
+        assert(outputs.keys.contains("logits"))
+
+        let scores = outputs["logits"]!
+        assert(scores.rank == 3)
+        let tokenIndex = tokenCount - 1
+        let nextTokenScores = scores[nil, tokenIndex, nil].expandingShape(at: 0)
+
+        assert(nextTokenScores.rank == 3)
+        assert(nextTokenScores.shape[0] == 1 && nextTokenScores.shape[1] == 1)
+        return nextTokenScores
+    }
+
+    @discardableResult
+    func generate(
+        config: GenerationConfig,
+        tokens: InputTokens,
+        callback: PredictionTokensCallback?
+    ) async -> GenerationOutput {
+        return await generate(
+            config: config,
+            tokens: tokens,
+            model: predictNextTokenScores,
+            callback: callback)
+    }
+}
+
+func main(prompt: String, maxLength: Int) async throws {
+    let modelURL = URL(fileURLWithPath: #filePath)
+        .deletingLastPathComponent()
+        .appendingPathComponent("GPT2Model")
+        .appendingPathExtension("mlmodelc")
+    print("Loading model from \(modelURL)")
+
+    let configuration = MLModelConfiguration()
+    let model = try! MLModel(contentsOf: modelURL, configuration: configuration)
+
+    let tokenizer = try await AutoTokenizer.from(pretrained: "gpt2")
+    let inputIds = tokenizer.encode(text: prompt)
+
+    let lm = try! GPT2TextGenerationModel.load_model()
+
+    var generationConfig = lm.defaultGenerationConfig
+    generationConfig.maxNewTokens = maxLength - inputIds.count
+    generationConfig.eosTokenId = tokenizer.eosTokenId
+    generationConfig.bosTokenId = tokenizer.bosTokenId
+
+    let generator = Generator(model: model, maxContextLength: maxContextLength)
+
+    _ = await generator.generate(
+        config: generationConfig,
+        tokens: inputIds
+    ) { tokens in
+        let text = tokenizer.decode(tokens: tokens)
+
+        // 改行なし、flush することでリアルタイムに出力できる
+        print(text, terminator: "")
+    }
+}
 
 guard CommandLine.arguments.count > 1 else {
     print("Usage: command <input text>")
@@ -8,7 +109,6 @@ guard CommandLine.arguments.count > 1 else {
 }
 
 let prompt = CommandLine.arguments[1]
-let lm = try! GPT2TextGenerationModel.load_model()
-let output = try! await lm.generate(config: lm.defaultGenerationConfig, prompt: prompt)
+let maxLength: Int = CommandLine.arguments.count > 2 ? Int(CommandLine.arguments[2])! : 100
 
-print("Output: \(output)")
+try! await main(prompt: prompt, maxLength: maxLength)
